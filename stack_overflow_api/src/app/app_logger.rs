eb98c6ca8831d175b732b5a082ae3d797d07d5b0
vkiryakov <vladimir.kiryakov@live.com>
2026-06-08 02:05:27 +0300

    finish logger

--- stack_overflow_api/src/app/app_logger.rs
+++ stack_overflow_api/src/app/app_logger.rs
@@ -4,7 +4,11 @@ pub fn init_logger(app_config: &AppConfig) {
     let env_filter =
         tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into());
 
-    let builder = tracing_subscriber::fmt().with_env_filter(env_filter);
+    let builder = tracing_subscriber::fmt()
+        .with_env_filter(env_filter)
+        .with_target(true)
+        .with_file(true)
+        .with_line_number(true);
 
     match app_config.get_app_env() {
         AppEnv::Development => builder.pretty().init(),
